58dedf2538e990cffd888f2bb08e87d456679072
F4A3VR1 <shubham.kanse@fiserv.com>
2023-05-12 17:53:44 +0530

    swagger-ui test2

--- assets/swagger/Release-swagger/swagger-initializer.js
+++ assets/swagger/Release-swagger/swagger-initializer.js
@@ -3,7 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "http://127.0.0.1:5500/assets/swagger/Release-swagger/23.06.yaml",
+    url: "http://127.0.0.1:5500/assets/swagger/Release-swagger/23.07.yaml",
+    url: "https://raw.githubusercontent.com/Fiserv/firstvision-emea/develop-qa/assets/swagger/Release-swagger/23.06.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
